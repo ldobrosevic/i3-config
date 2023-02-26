862c42e4c13cbaddcc909657a1034c60e8f8a14a
Lovro Dobrosevic <lovro.dobrosevic@gmail.com>
2023-02-27 00:56:45 +0100

    install feh

--- install-i3.sh
+++ install-i3.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail # fail on error and report it, debug all lines
 sudo -n true
 
 sudo apt-get update
+sudo apt-get install feh
 sudo apt-get install kitty
 sudo apt-get install i3
 cp config /home/*/.config/i3/

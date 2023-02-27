3620c3f6b08162f61b13fa337cff1b4fa011ac4a
Lovro Dobrosevic <lovro.dobrosevic@gmail.com>
2023-02-27 01:36:03 +0100

    add i3status config, enable bar

--- install-i3.sh
+++ install-i3.sh
@@ -4,11 +4,13 @@ set -eu -o pipefail # fail on error and report it, debug all lines
 sudo -n true
 
 sudo apt-get update
+sudo apt-get install pulseaudio
 sudo apt-get install feh
 sudo apt-get install kitty
 sudo apt-get install i3
 
 mkdir -p ~/Pictures/i3 && cp wallpaper.jpg ~/Pictures/i3/
 mkdir -p ~/.config/i3 && cp config ~/.config/i3/
+mkdir -p ~/.config/i3status && cp i3status_config ~/.config/i3status/config 
 
 echo "Hello World"
